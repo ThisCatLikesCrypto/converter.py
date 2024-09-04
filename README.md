2b790d8c38c6ec7dc5483f04f40aec1c83720734
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-09-04 10:39:04 +0100

    add heic support + add a bunch of others to 'supported'

--- README.md
+++ README.md
@@ -3,14 +3,16 @@ A simple python script to act as a 'universal converter' for file types
 Usage: ```python3 converter.py [input file] [output file]```
 
 # dependencies
-A recent python3, and ffmpeg (and Windows or Linux, I can't build for MacOS)
+A recent python3  
+Ffmpeg (and Windows or Linux, I can't build for MacOS)  
+and ```pip install heif-convert```
 
 ## installing ffmpeg
-On Windows, type ```winget install ffmpeg``` into the command prompt. K-Lite Codec Pack is recommended.
+On Windows, type ```winget install ffmpeg``` into the command prompt.  
 Most Linux distros have ffmpeg installed by default.
 
 # supported file types
-Video: mov, mp4, avi, and webm
-Audio: mp3, ogg, wav, flac, aac, wma, aiff, and m4a  
-Image: jpg, jpeg, png, webp, avif, jxl, gif, bmp, svg, and tiff  
+Video: mov, mp4, avi, webm, mk4, flv, wmv, m4v, f4v, and mpeg  
+Audio: mp3, ogg, wav, flac, aac, wma, aiff, m4a, mka, opus, and alac  
+Image: jpg, jpeg, png, webp, avif, jxl, heic (convert from only), heif (convert from only), gif, bmp, svg, ico and tiff  
 Font: ttf, woff (convert to only), woff2, eot (convert to only), svg (convert to only), otf (convert from (to eot and woff) only)
--- converter.py
+++ converter.py
@@ -4,11 +4,29 @@ import platform
 
 iswindows = platform.uname().system.startswith('Win')
 
-ffmpegExtensions = ["mov", "mp4", "webm", "avi", 
-                    "jpg", "jpeg", "png", "webp", "jxl", "avif", "gif", "bmp", "svg", "tiff",
-                    "mp3", "ogg", "wav", "flac", "aac", "wma", "aiff", "m4a"]
+ffmpegExtensions = ["mov", "mp4", "webm", "avi", "mkv", "flv", "wmv", "m4v", "f4v", "mpeg"
+                    "jpg", "jpeg", "png", "webp", "jxl", "avif", "gif", "bmp", "svg", "tiff", "ico",
+                    "mp3", "ogg", "wav", "flac", "aac", "wma", "aiff", "m4a", "mka", "opus", "alac"]
+heifConvertInExtensions = ["heic", "heif"]
+heifConvertOutExtensions = ["jpg", "png", "webp", "gif", "tiff", "bmp", "ico"]
 webifyInExtensions = ["ttf", "otf"]
 webifyOutExtensions = ["woff", "eot", "svg"]
+
+def heifConvertandFFmpegC(inputFile: str, outputFile: str, inputExt: str, outputExt: str):
+    print(f"\033[38;5;117mheif-convert does not support converting to that extension, however ffmpeg does.\nConverting {inputExt} to png with heif-convert and then to {outputExt} with ffmpeg.\033[0m")
+    tempfile = inputFile.replace(inputExt, "png")
+    # Workaround for when there is already a file with the same name as the tempfile in the directory
+    if tempfile in os.listdir():
+        temperfile = tempfile.replace("png", "TEMPORARILYRENAMED.png").replace("PNG", "TEMPORARILYRENAMED.PNG")
+        os.rename(tempfile, temperfile)
+    else:
+        temperfile = None
+    os.system(f'heif-convert {inputFile} -f png')
+    ffmpegC(tempfile, outputFile, 7)
+    os.remove(tempfile)
+    # Workaround above, pt2
+    if temperfile in os.listdir():
+        os.rename(temperfile, tempfile)
 
 def ffmpegC(inputFile, outputFile, effortLevel):
     os.system(f'ffmpeg -i {inputFile} {outputFile} -effort {effortLevel}')
@@ -75,10 +93,21 @@ def main():
     
     print(f"Input File: {inputFile} (type {inputExt})\nOutput File: {outputFile} (type {outputExt})")
 
+    if inputFile not in os.listdir():
+        print("bruh the input file doesn't exist")
+        sys.exit()
+    elif outputFile in os.listdir():
+        print("bruh the output file already exists")
+        sys.exit()
+
     if inputExt.lower() in ffmpegExtensions and outputExt.lower() in ffmpegExtensions:
         ffmpegC(inputFile, outputFile, effortLevel)
-    elif inputExt.lower() == "woff":
-        print("fuck")
+    elif inputExt.lower() in heifConvertInExtensions and outputExt.lower() in heifConvertOutExtensions:
+        os.system(f'heif-convert {inputFile} -f {outputExt}')
+        if outputFile != inputFile.replace(inputExt, outputExt):
+            os.rename(inputFile.replace(inputExt, outputExt), outputFile)
+    elif inputExt.lower() in heifConvertInExtensions and outputExt.lower() in ffmpegExtensions:
+        heifConvertandFFmpegC(inputFile, outputFile, inputExt, outputExt)
     elif inputExt.lower() in webifyInExtensions and outputExt.lower() in webifyOutExtensions:
         webifyC(inputFile, outputFile, outputExt)
     elif inputExt.lower() == "woff2" and outputExt.lower() == "ttf":
